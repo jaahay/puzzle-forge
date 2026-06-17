c1d591876bcf064104d1ff68c545d54f210d0971
James Hay <jaahay@users.noreply.github.com>
2026-06-16 18:37:58 -0700

    Load Sudoku polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { render } from "preact";
 import { App } from "./App";
 import "./styles.css";
 import "./site/solitaire.css";
+import "./site/sudoku.css";
 import "./site/base.css";
 import "./site/tabs.css";
 import "./site/views.css";
@@ -9,8 +10,6 @@ import "./site/footer.css";
 
 const root = document.getElementById("app");
 
-if (!root) {
-  throw new Error("App root element was not found.");
+if (root) {
+  render(<App />, root);
 }
-
-render(<App />, root);
