07a7e044626f004b86b1c98169b6c09110081a26
James Hay <jaahay@users.noreply.github.com>
2026-08-28 14:31:08 -0700

    Load prospective settings styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./site/views.css";
 import "./site/footer.css";
 import "./site/app-shell.css";
 import "./site/workspace.css";
+import "./site/next-puzzle.css";
 import "./site/immersive.css";
 import "./site/mobile-workspace.css";
 import "./site/grid.css";
