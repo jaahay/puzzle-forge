96c82892837369cb7ce522809b68038a28863829
James Hay <jaahay@users.noreply.github.com>
2026-08-29 16:26:31 -0700

    Route grid and Solitaire puzzles to focused workspaces

--- src/components/PuzzleWorkspace.tsx
+++ src/components/PuzzleWorkspace.tsx
@@ -1,9 +1,10 @@
 import type { FunctionComponent } from "preact";
 import type { PuzzleId } from "../catalog/types";
+import { GridPuzzleWorkspace } from "./GridPuzzleWorkspace";
 import { ImageTilePuzzleWorkspace } from "./ImageTilePuzzleWorkspace";
 import { JigsawWorkspace } from "./JigsawWorkspace";
 import type { PuzzleWorkspaceProps } from "./PuzzleWorkspace.types";
-import { StandardPuzzleWorkspace } from "./StandardPuzzleWorkspace";
+import { SolitaireWorkspace } from "./SolitaireWorkspace";
 import { SudokuWorkspace } from "./SudokuWorkspace";
 
 export type { PuzzleWorkspaceProps } from "./PuzzleWorkspace.types";
@@ -12,18 +13,18 @@ type WorkspaceComponent = FunctionComponent<PuzzleWorkspaceProps>;
 
 const workspaceRegistry = {
   sudoku: SudokuWorkspace,
-  nonogram: StandardPuzzleWorkspace,
-  "word-guess": StandardPuzzleWorkspace,
-  "logic-grid": StandardPuzzleWorkspace,
-  "klondike-solitaire": StandardPuzzleWorkspace,
-  "peg-solitaire": StandardPuzzleWorkspace,
-  futoshiki: StandardPuzzleWorkspace,
-  kenken: StandardPuzzleWorkspace,
-  minesweeper: StandardPuzzleWorkspace,
+  nonogram: GridPuzzleWorkspace,
+  "word-guess": GridPuzzleWorkspace,
+  "logic-grid": GridPuzzleWorkspace,
+  "klondike-solitaire": SolitaireWorkspace,
+  "peg-solitaire": GridPuzzleWorkspace,
+  futoshiki: GridPuzzleWorkspace,
+  kenken: GridPuzzleWorkspace,
+  minesweeper: GridPuzzleWorkspace,
   jigsaw: JigsawWorkspace,
   "tile-swap": ImageTilePuzzleWorkspace,
   "sliding-puzzle": ImageTilePuzzleWorkspace,
-  slitherlink: StandardPuzzleWorkspace,
+  slitherlink: GridPuzzleWorkspace,
 } satisfies Record<PuzzleId, WorkspaceComponent>;
 
 export const PuzzleWorkspace = (props: PuzzleWorkspaceProps) => {
