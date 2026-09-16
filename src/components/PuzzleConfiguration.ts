5fddad748839e12a3d7effa410f2e3ea205921a6
James Hay <jaahay@users.noreply.github.com>
2026-09-15 23:20:41 -0700

    Remove obsolete immediate configuration export

--- src/components/PuzzleConfiguration.ts
+++ src/components/PuzzleConfiguration.ts
@@ -1,3 +1,2 @@
 export { BottomPuzzleConfiguration } from "./BottomPuzzleConfiguration";
-export { ImmediateTopPuzzleConfiguration } from "./ImmediateTopPuzzleConfiguration";
 export { TopPuzzleConfiguration } from "./TopPuzzleConfiguration";
